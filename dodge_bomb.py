f3484a836b1ff1ce3c910936b12c0fe0cb43ed8b
tanaka <c0a23098b2@edu.teu.ac.jp>
2024-04-23 17:30:13 +0900

    追加機能3 訂正

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -9,7 +9,7 @@ WIDTH, HEIGHT = 1600, 900
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 screen =pg.display.set_mode((WIDTH,HEIGHT))
 
-def check_bound(obj_rct:pg.Rect) -> tuple[bool,bool]:
+def check_bound(obj_rct:pg.Rect) -> tuple[bool,bool]: #衝突識別関数
     yoko, tate =True, True
     if obj_rct.left < 0 or WIDTH < obj_rct.right:
         yoko = False
@@ -28,7 +28,6 @@ def Draw_rect():
     pg.draw.rect(over_img, (0, 0, 0),(0, 0, 1600, 900) , 0)
     over_img.set_alpha( 150 )
     screen.blit(over_img,[0, 0])
-    print("owata")
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -46,7 +45,6 @@ def main():
     bd_rct = bd_img.get_rect()
     bd_rct.center = random.randint(0,WIDTH),random.randint(0,HEIGHT)
     vx, vy = +5 , +5
-    toki = 0
     move = {
         pg.K_UP:(0,-5),
         pg.K_DOWN:(0,+5),
@@ -61,12 +59,11 @@ def main():
                 Draw_rect() #関数呼び出し
                 fonto = pg.font.Font(None, 100) #文字の大きさ
                 txt = fonto.render("Game over", True, (255,255,255)) #文字の描画
-                screen.blit(txt, [610,420]) #blit
-                screen.blit(kk_n, [480,380]) #こうかとんのはりつけ
+                screen.blit(txt, [610,420]) #文字の貼り付け
+                screen.blit(kk_n, [480,380]) #こうかとんの貼り付け
                 screen.blit(kk_n, [1010,380])
                 pg.display.update() #画面の更新
-                print("Geme over")
-                time.sleep( 5 )
+                time.sleep( 5 ) #5秒で終了させる
                 return
         
         screen.blit(bg_img, [0, 0]) 
@@ -74,20 +71,24 @@ def main():
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
         for k,v in move.items():
-            if key_lst[k]:
+            if key_lst[k]: #こうかとんの移動速度
                 sum_mv[0] += v[0]
                 sum_mv[1] += v[1]
         kk_rct.move_ip(sum_mv)
-        if check_bound(kk_rct) != (True, True):
+
+        if check_bound(kk_rct) != (True, True): #こうかとんと壁の衝突判定
             kk_rct.move_ip(-sum_mv[0],-sum_mv[1])
+
         screen.blit(kk_img, kk_rct)
         bd_rct.move_ip(vx,vy)
         screen.blit(bd_img,bd_rct)
-        yoko, tate =check_bound(bd_rct)
+
+        yoko, tate =check_bound(bd_rct) #爆弾と壁の衝突判定
         if not yoko:
             vx *= -1
         if not tate:
             vy *= -1
+
         pg.display.update()
         tmr += 1
         clock.tick(50)
